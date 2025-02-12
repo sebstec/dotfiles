a1d22401d1a420c218aab628b997f58922ff7263
sebste <>
2025-02-12 19:05:14 +0100

    feat: add dirs to tmux sessionizer

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/PhpstormProjects -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/PhpstormProjects ~/cysec/after-studies/docker -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
